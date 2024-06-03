5db148163ccc9d28ad3acdc1c62796b692340bfe
Aniruddha <aniruddhaaru06@gmail.com>
2024-06-03 15:55:14 +0530

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full bg-gradient-to-b from-gray-900 to to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-6">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+        <p className="text-xl mt-19">
+        As a frontend developer , I have a strong foundation in web development technologies such as HTML, CSS, and JavaScript. 
+        I specialize in creating engaging and user-friendly interfaces by leveraging frameworks like React.js With hands-on experience in building responsive websites and web application. 
+        Additionally, I am proficient and have a good understanding of modern frontend development workflows and best practices. 
+        My goal is to continue honing my skills and staying updated with the latest advancements in frontend technologies to deliver innovative solutions that enhance user experiences.
+        </p>
+        <br />
+       
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import OIP from "../assets/portfolio/OIP.jpeg";
+import todo from "../assets/portfolio/todo.jpeg";
+import Next from "../assets/portfolio/Next.jpg.png";
+
+const Portfolio = () => {
+  const portfolio = [
+    {
+      id: 1,
+      src: OIP,
+      demo: "https://Aniruddhaaru.github.io/Tictactoe",
+      code: "https://github.com/Aniruddhaaru/Tictactoe",
+    },
+    {
+      id: 2,
+      src: todo,
+      demo: "https://example.com/demo2",
+      code: "https://github.com/Aniruddhaaru/React-todolist",
+    },
+    {
+      id: 3,
+      src: Next,
+      demo: "https://example.com/demo3",
+      code: "https://github.com/Aniruddhaaru/Next-todolist",
+    },
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolio.map(({ id, src, demo, code }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md h-60 w-full object-cover duration-200 hover:scale-105 cursor-pointer"
+              />
+              <div className="flex items-center justify-center">
+                <a href={demo} target="_blank" rel="noopener noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-center bg-gray-800 rounded-md">
+                  Demo
+                </a>
+                <a href={code} target="_blank" rel="noopener noreferrer" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-center bg-gray-800 rounded-md">
+                  Code
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
